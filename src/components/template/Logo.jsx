a3587b0a8c32ed057abb066e88dc3d9f7fff5bc2
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-08-30 19:58:38 -0300

    Componente Logo

--- src/components/template/Logo.jsx
+++ src/components/template/Logo.jsx
@@ -1,4 +1,11 @@
 import "./Logo.css";
+import logo from "../../assets/img/code.svg";
 import React from "react";
 
-export default (props) => <aside className="logo">Logo</aside>;
+export default (props) => (
+  <aside className="logo">
+    <a href="/" className="logo">
+      <img src={logo} alt="logo" />
+    </a>
+  </aside>
+);
