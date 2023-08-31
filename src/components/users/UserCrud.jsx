be9f83b356995ec73518f885136a4bc4ce686ca9
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-08-31 19:36:45 -0300

    Criação do estado inicial

--- src/components/users/UserCrud.jsx
+++ src/components/users/UserCrud.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import Main from "../template/Main";
 
 const headerProps = {
@@ -7,7 +8,35 @@ const headerProps = {
   subtitle: "Cadastro de usuários: Incluir, Listar, Alterar e Excluir!",
 };
 
+// estado inicial
+const baseUrl = "http://localhost:3001/users";
+const initialState = {
+  user: { name: "", email: "" },
+  list: [],
+};
 export default class UserCrud extends Component {
+  state = { ...initialState };
+
+  clear() {
+    this.setState({ user: initialState.user });
+  }
+
+  save() {
+    const user = this.state.user;
+    const method = user.id ? "put" : "post";
+    const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
+    axios[method](url, user).then((resp) => {
+      const list = this.getUpdateList(resp.data);
+      this.setState({ user: initialState.user, list });
+    });
+  }
+
+  getUpdateList(user) {
+    const list = this.setState.filter((u) => u.id !== user.id);
+    list.unshift(user);
+    return list;
+  }
+
   render() {
     return <Main {...headerProps}>Cadastro de Usuário</Main>;
   }
